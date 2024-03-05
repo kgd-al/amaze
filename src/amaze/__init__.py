e03a222bea1b54e8e70ed0c85f1afd97a1d26369
Kevin Godin-Dubois <godindubois@gmail.com>
2024-03-05 10:33:21 +0100

    Fixed stupid mistake

--- src/amaze/__init__.py
+++ src/amaze/__init__.py
@@ -4,4 +4,4 @@
 from amaze.simu import Maze, Robot, Simulation, InputType, OutputType, load
 from amaze.visu.widgets.maze import MazeWidget
 from amaze.visu.widgets import application
-from amaze.bin.amaze import main as amaze_main
+from amaze.bin.main import main as amaze_main
